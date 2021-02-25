573f1d1ff4dea669caa482883c33c7666b65ae8a
Prabhakar Thangavel <prabhakart.ge@gmail.com>
2021-02-25 20:36:00 +0530

    first commit

---
+++ src/main/java/com/mobileapp/bingewatch/BingeWatchApplication.java
@@ -0,0 +1,13 @@
+package com.mobileapp.bingewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingeWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingeWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobileapp/bingewatch/BingeWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileapp.bingewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingeWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
